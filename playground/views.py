279471dc10c10e76b6b655777e5cc6ccde276339
OMAR BEEN HASIB <adonomar02@gmail.com>
2025-03-24 00:00:05 +0200

    Commit with custom admin site

--- playground/views.py
+++ playground/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def calculate():
-    x = 1
-    y = 2
-    return x
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product, Customer, Collection, Order, OrderItem
+from django.db import connection
+from tags.models import TaggedItem
 
 
 def say_hello(request):
-    x = calculate()
+    with connection.cursor() as cursor:
+        cursor.callproc('get_customers',[1,2,3])
     return render(request, 'hello.html', {'name': 'Mosh'})
+    
